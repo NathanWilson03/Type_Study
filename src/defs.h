d876e9a48c89ee76f67e576e845faa6e6cddf998
natewilson <natewilson869@gmail.com>
2026-01-12 22:57:53 -0500

    Need to fix a lot

--- src/defs.h
+++ src/defs.h
@@ -1,9 +1,13 @@
 #define G_WIDTH 800
 #define G_HEIGHT 600
+#define MY_FONT "fonts/Roboto-Regular.ttf" 
 
 typedef struct {
     SDL_Window *window;
-    SDL_Surface *surface;
+    SDL_Surface *text_surface;
+    SDL_Renderer *renderer;
+    TTF_Font *font;
+    SDL_Texture *text_texture;
 } Game;
 
 
--- src/init.c
+++ src/init.c
@@ -3,10 +3,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <SDL.h> // use default sdl for now 
+#include <SDL_ttf.h>
 
 #include "defs.h"
 
 int open_window(Game *g);
+int begin_display(Game *g, char *str);
 void close_window(Game *g);
 
 
@@ -24,8 +26,9 @@ int main() {
 }
 
 int open_window(Game *g) {
-
-    int render_flags, window_color, quit = 0;
+    
+    char *str = "Welcome to Type_Study";
+    int render_flags, window_color, quit = 0, i = 0;
     SDL_Event e;
 
     // allows for hardware acceleration
@@ -33,6 +36,11 @@ int open_window(Game *g) {
     // need to initialize to use library   
     if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
         fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }    
+    if( TTF_Init() < 0 ) {
+        fprintf(stderr, "Could not create text: %s\n", TTF_GetError());
+        SDL_Quit();
         return 1;
     }
 
@@ -43,39 +51,83 @@ int open_window(Game *g) {
         fprintf(stderr, "Failed to open window: %s\n", SDL_GetError());
         return 1;
     }
-    // getting the surface allows to draw
-    // maybe could use renderer as well
-    g->surface = SDL_GetWindowSurface( g->window );
-    if( !g->surface ) {
+
+    g->renderer = SDL_CreateRenderer( g->window , -1, 0 );
+    if( !g->renderer ) {
         fprintf(stderr, "Failed to create surface: %s\n", SDL_GetError());
         return 1;
     }
 
-    // for window color param in fill rect func 
-    window_color = SDL_MapRGB(g->surface->format, 0x21, 0x21, 0x21);
+        SDL_SetRenderDrawColor(g->renderer, 35, 31, 31, 255);
+        while( i < strlen(str) + 1 ) {
+        
+        begin_display(g, str);
+        i++;
+        }
+        SDL_UpdateWindowSurface( g->window );
 
-    SDL_FillRect( g->surface, NULL, window_color );
-    SDL_UpdateWindowSurface( g->window );
-
-    // main game loop
-    while( quit == 0 ) {
-        while( SDL_PollEvent(&e) ) {
-            if( e.type  == SDL_QUIT ) {
-                // will not return until quit is entered
-                quit = 1;
+        // main game loop
+        // everything that happens in this function 
+        // stays on the screen 
+        while( quit == 0 ) {
+            while( SDL_PollEvent(&e) ) {
+                if( e.type  == SDL_QUIT ) {
+                    // will not return until quit is entered
+                    quit = 1;
+                }
             }
         }
+    return 0;
+}
+
+int begin_display(Game *g, char *str) {
+    // need to add a fonts dir for portability  
+    int i, posX = 10, loaded_char = 1;
+    char buff[128];
+
+    strncpy(buff, str, loaded_char);
+    buff[loaded_char] = '\0';
+    g->font = TTF_OpenFont("/System/Library/Fonts/Monaco.ttf", 30);
+    if( !g->font ) {
+        fprintf(stderr, "Font init failed: %s\n", TTF_GetError());
+        return 1;
     }
+    SDL_Color text_color = { 0, 0, 0, 255 }; 
+
+    g->text_surface = TTF_RenderText_Solid(g->font, buff, text_color);
+        if( !g->text_surface ) {
+            fprintf(stderr, "Text surface failed: %s\n", SDL_GetError());
+            return 1;
+        }
+
+        g->text_texture = SDL_CreateTextureFromSurface(g->renderer, g->text_surface);
+
+        if( !g->text_texture ) {
+            fprintf(stderr, "Text texture failed to render: %s\n", SDL_GetError());
+            return 1;
+        }
+        SDL_Rect text_rect = { 10, 10, g->text_surface->w, g->text_surface->h };
+        SDL_RenderCopy(g->renderer, g->text_texture, NULL, &text_rect);
+
+        // shows frame after text 
+        // put on
+        SDL_RenderPresent(g->renderer);
+        loaded_char++;
+        SDL_Delay(100);
+
     return 0;
 }
 
 void close_window(Game *g) {
 
-    SDL_FreeSurface( g->surface );
-    g->surface = NULL;
+    SDL_FreeSurface( g->text_surface);
+    g->text_surface = NULL;
 
     SDL_DestroyWindow( g->window );
     g->window = NULL;
+
+    SDL_DestroyRenderer( g->renderer );
+    g->renderer = NULL;
 
     SDL_Quit();
 
