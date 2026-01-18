58780eb934a4b9bdc125df71df38e182c447b598
natewilson <natewilson869@gmail.com>
2026-01-17 19:51:24 -0500

    started fresh. Need work on button

--- src/defs.h
+++ src/defs.h
@@ -1,13 +1,28 @@
+#define SUCCESS 0
+#define FAILURE 1
+#define BUFF_SIZE 128
 #define G_WIDTH 800
 #define G_HEIGHT 600
-#define MY_FONT "fonts/Roboto-Regular.ttf" 
+#define B_WIDTH 200
+#define B_HEIGHT 75
+#define MY_FONT "../fonts/roboto/Roboto-Regular.ttf" 
+#define SDL_FLAGS (SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO )
+#define WINDOW_FLAGS (SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE ) 
 
 typedef struct {
     SDL_Window *window;
-    SDL_Surface *text_surface;
     SDL_Renderer *renderer;
+    SDL_Surface *text_surface;
     TTF_Font *font;
     SDL_Texture *text_texture;
+    
 } Game;
+
+typedef struct {
+
+    SDL_Renderer *renderer;
+
+} Button;
+
 
 
--- src/init.c
+++ src/init.c
@@ -2,169 +2,190 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <SDL.h> // use default sdl for now 
+/* default sdl library for now */
+#include <SDL.h>
 #include <SDL_ttf.h>
-
 #include "defs.h"
-static int loaded_char_str1 = 1; 
-static int loaded_char_str2 = 1;
-int init_fields(Game *g);
-int open_window(Game *g);
-int begin_display(Game *g, const char *str);
-void close_window(Game *g);
 
+/* could add audio later */
+
+int open_window(Game *);
+int close_window(Game *);
+int start_game(Game *, int *, int * ); 
 
 int main() {
+        
+    Game *g = malloc( sizeof(Game) );
+    open_window(g);
 
-    Game g = {0};
-    if( open_window(&g) ) {
-        return 1;
-    }
-
-    close_window(&g);
+    close_window(g);
 
     return 0;
-
 }
-
-int init_fields(Game *g) {
-    int render_flags = SDL_RENDERER_ACCELERATED;
-   
-    if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-        fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
-        return 1;
-    }    
-    if( TTF_Init() < 0 ) {
-        fprintf(stderr, "Could not create text: %s\n", TTF_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    g->window = SDL_CreateWindow("Type_Study", SDL_WINDOWPOS_CENTERED, 
-            SDL_WINDOWPOS_CENTERED, G_WIDTH, G_HEIGHT, SDL_WINDOW_SHOWN);
-
-    if( !g->window ) {
-        fprintf(stderr, "Failed to open window: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    g->renderer = SDL_CreateRenderer( g->window , -1, render_flags );
-    if( !g->renderer ) {
-        fprintf(stderr, "Failed to create surface: %s\n", SDL_GetError());
-        return 1;
-    }
-
-
-    g->font = TTF_OpenFont("/System/Library/Fonts/Monaco.ttf", 30);
-    if( !g->font ) {
-        fprintf(stderr, "Font init failed: %s\n", TTF_GetError());
-        return 1;
-    }
-    return 0;
-}
-
-
-
 
 int open_window(Game *g) {
-
-    const char *welcome_str = "Welcome to Type_Study. ";
-    const char *info_str = "Please enter your User Name.";
-    int window_color, quit = 0, i = 0;
+    
+    int quit = 0, num_chr_disp1 = 1, num_chr_disp2 = 1;  
     SDL_Event e;
 
-    init_fields(g);
+    /* allow for window functionality */
+    if( SDL_Init(SDL_FLAGS) != 0) {
+        fprintf(stderr, "Window failed to init: %s\n", SDL_GetError());
+        return FAILURE;
+    }
+    if( TTF_Init() != 0 ) {
+        fprintf(stderr, "Text failed to init: %s\n", SDL_GetError());
+        return FAILURE;
+    }
 
-    // need to initialize to use library   
-       SDL_UpdateWindowSurface( g->window );
+    g->window = SDL_CreateWindow("Type_Study", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                 G_WIDTH, G_HEIGHT, WINDOW_FLAGS);
+    if( !g->window ) {
+        fprintf(stderr, "Window could not be created: %s\n", SDL_GetError());
+        return FAILURE;
+    }
 
-    // main game loop
-    // everything that happens in this function 
-    // stays on the screen 
+    g->renderer =  SDL_CreateRenderer(g->window, -1, SDL_RENDERER_ACCELERATED);
+
+    if( !g->renderer ) {
+        fprintf(stderr, "Renderer failed to begin display: %s\n", SDL_GetError());
+        return FAILURE;
+    }
+    /* change to be compatable with more fonts */
+    g->font = TTF_OpenFont( MY_FONT, 48 );
+
+    if( !g->font ) {
+        fprintf(stderr, "TTF Font failed to init: %s\n", TTF_GetError());
+        return FAILURE;
+    }
+   
+
+    SDL_SetRenderDrawColor( g->renderer, 0, 0, 0, 0 );
+
     while( quit == 0 ) {
         while( SDL_PollEvent(&e) ) {
-            if( e.type  == SDL_QUIT ) {
-                // will not return until quit is entered
+            if( e.type == SDL_QUIT ) {
                 quit = 1;
             }
         }
-    SDL_SetRenderDrawColor(g->renderer, 35, 31, 31, 255);
+    /* drawing a white screen */
     SDL_RenderClear(g->renderer);
-    
-    begin_display(g, welcome_str);
+
+    start_game(g, &num_chr_disp1, &num_chr_disp2);
+
     SDL_RenderPresent(g->renderer);
+    
+    /* freeing after presenting */
+    SDL_FreeSurface(g->text_surface);
+    g->text_surface = NULL;
+
+    SDL_DestroyTexture(g->text_texture);
+    g->text_texture = NULL;
+    
+
+
     }
-    return 0;
+
+   return SUCCESS;
 }
 
-int get_user_info(Game *g) {
-    return 0;
-}
+int start_game( Game *g, int *num_chr_disp1, int *num_chr_disp2 ) {
+    
+    char *welcome_str1 = "Welcome to Type_Study.";
+    char *welcome_str2 = "Please";
+    char buff[BUFF_SIZE];
+    int pos_x, pos_y;
+    SDL_Color text_color = { 255 ,255 ,255 ,255 }; /* black */
 
-
-
-int begin_display(Game *g, const char *str) {
-    // need to add a fonts dir for portability  
-    int i, posY = 10, flag = 0;
-
-    char buff[128];
-    const char *str2 = "Please enter your username";
-    if( loaded_char_str1 > strlen(str) ) {
-        str = str2;
-        posY = 50;
-        strncpy(buff, str, loaded_char_str2);
-        buff[loaded_char_str2] = '\0';
-        loaded_char_str2++;
+    /* initial prompt has typing effect */
+    if( *num_chr_disp1 < strlen(welcome_str1) ) {
+        
+        buff[*num_chr_disp1] = '\0';
+        strncpy(buff, welcome_str1, *num_chr_disp1);
+        pos_x = 150;
+        pos_y = 50;
+        (*num_chr_disp1)++;
+    
     } else {
 
-    strncpy(buff, str, loaded_char_str1);
-    buff[loaded_char_str1] = '\0';
-    loaded_char_str1++;
+        if( *num_chr_disp1 == strlen(welcome_str1) ){
+            SDL_Delay(200);
+            (*num_chr_disp1)++;
+        }
+
+        buff[*num_chr_disp2] = '\0';
+        strncpy(buff, welcome_str2, *num_chr_disp2);
+        pos_x = 300;
+        pos_y = 100;
+
+        if( *num_chr_disp2 < strlen(welcome_str2) ) {
+        (*num_chr_disp2)++;
+        }
     }
 
-    SDL_RenderClear(g->renderer);
-    SDL_Color text_color = { 255, 255, 255, 255 }; 
 
-    g->text_surface = TTF_RenderText_Solid(g->font, buff, text_color);
+
+ 
+    /* stored in CPU */ 
+    g->text_surface = TTF_RenderText_Solid( g->font, buff, text_color );
     if( !g->text_surface ) {
-        fprintf(stderr, "Text surface failed: %s\n", SDL_GetError());
-        return 1;
+        fprintf(stderr, "Text Surface failed to init: %s\n", TTF_GetError());
+        return FAILURE;
     }
 
     g->text_texture = SDL_CreateTextureFromSurface(g->renderer, g->text_surface);
-
     if( !g->text_texture ) {
-        fprintf(stderr, "Text texture failed to render: %s\n", SDL_GetError());
-        return 1;
+        fprintf(stderr, "Text Texture failed to init: %s\n", TTF_GetError());
+        return FAILURE;
     }
-    SDL_Rect text_rect = { 10, posY, g->text_surface->w, g->text_surface->h };
+
+    SDL_Rect text_rect = { pos_x, pos_y, g->text_surface->w, g->text_surface->h };
     SDL_RenderCopy(g->renderer, g->text_texture, NULL, &text_rect);
-
-    // shows frame after text 
-    // put on
-    SDL_RenderPresent(g->renderer);
+    
+    if( *num_chr_disp2 <= strlen(welcome_str2) ) 
     SDL_Delay(100);
-    // free the surface created 
-    SDL_FreeSurface(g->text_surface);
-    SDL_DestroyTexture(g->text_texture);
 
-    return 0;
+    if( *num_chr_disp2 > strlen(welcome_str2) ) {
+    /* make the buttons for login signup */
+    SDL_Rect b_rect = { 300, 300, B_WIDTH, B_HEIGHT };
+    SDL_SetRenderDrawColor(g->renderer, 200, 200, 200, 255);
+    SDL_RenderFillRect(g->renderer, &b_rect);
+
+
+    }
+
+
+    
+    
+    
+    return SUCCESS;
 }
 
-void close_window(Game *g) {
 
-    SDL_FreeSurface( g->text_surface);
-    g->text_surface = NULL;
 
-    SDL_DestroyWindow( g->window );
+
+
+
+
+
+int close_window(Game *g) {
+     
+    SDL_DestroyWindow(g->window);
     g->window = NULL;
-
-    SDL_DestroyRenderer( g->renderer );
+    SDL_DestroyRenderer(g->renderer);
     g->renderer = NULL;
-
     SDL_Quit();
 
+    free(g);
+    g = NULL;
+
+    return SUCCESS;
 
 }
+ 
+
+
+
+
 
 
